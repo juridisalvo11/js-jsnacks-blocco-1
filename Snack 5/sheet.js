a4d7aa164f7b0921e8026fda227ebfafd613c871
Juri Di salvo <Juridisalvo11@gmail.com>
2020-04-23 21:47:19 +0200

    Snack 5 - Snack 6

--- Snack 5/sheet.js
+++ Snack 5/sheet.js
@@ -2,8 +2,8 @@
 for (var i = 0; i < 1; i++) {
     var numero = parseInt(prompt('Inserisci un numero'));
     //console.log(numero);
-
+//eleva il numero utente al cubo
     var risultato = Math.pow(numero, 3)
 }
-
+//stampa risultato
 console.log(risultato);
